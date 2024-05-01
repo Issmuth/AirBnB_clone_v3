971072fa592a9d5e59569097f9d36d1bbd527b9f
issmuth <ismael.mdev@gmail.com>
2024-05-01 14:32:42 +0300

    done

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,8 +5,8 @@ from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def clear(exc):
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,13 +1,43 @@
 #!/usr/bin/python3
 """Routes for the blueprint."""
 from api.v1.views import app_views
-from flask import jsonify
+from flask import Response
+from models import storage
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
+from models.user import User
 import json
 
-@app_views.route('/status')
+
+@app_views.route('/status', strict_slashes=False)
 def status():
     """Returns OK status in json format."""
-    stat = { 
+    stat = {
         "status": "OK"
     }
-    return jsonify(stat)
+    data = json.dumps(stat, indent=2) + '\n'
+    response = Response(response=data,
+                        status=200,
+                        mimetype='application/json')
+    return response
+
+
+@app_views.route('/stats')
+def stats():
+    """Return objects count of the classes."""
+    stat = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
+    }
+    data = json.dumps(stat, indent=2) + '\n'
+    response = Response(response=data,
+                        status=200,
+                        mimetype="application/json")
+    return response
