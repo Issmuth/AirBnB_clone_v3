5faea30c895f56d2a4ee32c0225a801f2f9357f6
issmuth <ismael.mdev@gmail.com>
2024-05-01 11:51:46 +0300

    done

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Routes for the blueprint."""
 from api.v1.views import app_views
-from flask import Response
+from flask import jsonify
 import json
 
 
@@ -11,7 +11,4 @@ def status():
     stat = {
         "status": "OK"
     }
-    data = json.dumps(stat, indent=2) + '\n'
-    resp = Response(response=data, status=200,
-                    mimetype="application/json")
-    return resp
+    return jsonify(stat)
