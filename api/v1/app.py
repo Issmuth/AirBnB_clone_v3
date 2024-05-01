eedd9d70953e4982ee334b772633e67971242991
issmuth <ismael.mdev@gmail.com>
2024-05-01 15:14:33 +0300

    Not Found

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """creates an app that registers blueprints."""
-from flask import Flask
+from flask import Flask, Response
 from models import storage
 from api.v1.views import app_views
+import json
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -12,6 +13,18 @@ app.register_blueprint(app_views)
 def clear(exc):
     """reloads storage using close method."""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(e):
+    error = {
+        "error": "Not found"
+    }
+    data = json.dumps(error, indent=2) + '\n'
+    response = Response(data,
+                        status=404,
+                        mimetype="application/json")
+    return response
 
 
 if __name__ == "__main__":
