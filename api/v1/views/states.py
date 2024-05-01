0b87388f3f73963f1571c47c8d8695ff0f87bfa6
issmuth <ismael.mdev@gmail.com>
2024-05-01 20:10:46 +0300

    done

---
+++ api/v1/views/states.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python3
+"""View for states."""
+from api.v1.views import app_views
+from models import storage
+from models.state import State
+import json
+from flask import Response, abort, jsonify, request
+
+
+@app_views.route('/states',
+                 strict_slashes=False,
+                 methods=['GET'])
+def states():
+    """retrieves all state objects."""
+    objects = storage.all(State)
+    state_dicts = []
+    for v in objects.values():
+        state_dicts.append(v.to_dict())
+
+    state_json = json.dumps(state_dicts, indent=2) + '\n'
+    response = Response(response=state_json,
+                        status=200,
+                        mimetype='application/json')
+    return response
+
+
+@app_views.route('/states/<state_id>',
+                 strict_slashes=False,
+                 methods=['GET'])
+def states_by_id(state_id):
+    """retrieve a state object using id."""
+    state = storage.get(State, state_id)
+    if state is not None:
+        data = json.dumps(state.to_dict(), indent=2) + '\n'
+        reponse = Response(response=data,
+                           status=200,
+                           mimetype='application/json')
+        return reponse
+    else:
+        abort(404)
+
+
+@app_views.route('/states/<state_id>',
+                 strict_slashes=False,
+                 methods=['DELETE'])
+def states_delete(state_id):
+    """Deletes a state object using id."""
+    state = storage.get(State, state_id)
+    if state is not None:
+        storage.delete(state)
+        storage.save()
+        return jsonify({}), 200
+    else:
+        abort(404)
+
+
+@app_views.route('/states',
+                 strict_slashes=False,
+                 methods=['POST'])
+def states_create():
+    """creates a state object."""
+    if not request.get_json():
+        abort(400, "Not a JSON")
+
+    if 'name' not in request.json:
+        abort(400, "Missing name")
+
+    name = request.get_json()['name']
+    state = State(name=name)
+    storage.new(state)
+    storage.save()
+    data = json.dumps(state.to_dict(), indent=2) + '\n'
+    reponse = Response(response=data,
+                       status=201,
+                       mimetype='application/json')
+    return reponse
+
+
+@app_views.route('/states/<state_id>',
+                 strict_slashes=False,
+                 methods=['PUT'])
+def state_update(state_id):
+    """updates a state objects."""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    state_update = request.get_json()
+    if not state_update:
+        abort(400, "Not a JSON")
+
+    for k, v in state_update.items():
+        skips = ["id", "created_at", "updated_at"]
+        if k not in skips:
+            setattr(state, k, v)
+
+    state.save()
+    data = json.dumps(state.to_dict(), indent=2) + '\n'
+    reponse = Response(response=data,
+                       status=200,
+                       mimetype='application/json')
+    return reponse
--- models/place.py
+++ models/place.py
@@ -33,10 +33,9 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, nullable=False, default=0)
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
-        reviews = relationship("Review", backref="place")
-        amenities = relationship("Amenity", secondary="place_amenity",
-                                 backref="place_amenities",
-                                 viewonly=False)
+        reviews = relationship("Review",  cascade='all, delete, delete-orphan', backref="place")
+        amenities = relationship("Amenity", secondary=place_amenity,
+                                 viewonly=False,  back_populates="place_amenities")
     else:
         city_id = ""
         user_id = ""
